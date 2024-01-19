610ff6bd629315b2a0a3a68b6b1671c5883d9a71
moeariyoshi <moe.ariyoshi@Michaelangelo.local>
2024-01-19 04:11:49 -0500

    create post model

--- sample/myapp/models.py
+++ sample/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+# To create and apply migrations:
+# python manage.py makemigrations
+# python manage.py migrate
