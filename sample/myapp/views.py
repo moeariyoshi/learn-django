63233b760ccb5ae311c0e8a1b0eeb45dda0b274e
moeariyoshi <moe.ariyoshi@Michaelangelo.local>
2024-01-19 04:11:58 -0500

    create the view

--- sample/myapp/views.py
+++ sample/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    latest_posts = Post.objects.order_by('-pub_date')[:5]
+    context = {'latest_posts': latest_posts}
+    return render(request, 'myapp/index.html', context)
